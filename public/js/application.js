57e863e7075ce2e92280de6a6551336c7c21427d
Walter Zimmerman <zimmermanw84@gmail.com>
2015-02-28 14:48:29 -0800

    Start Card Fade added

--- public/js/application.js
+++ public/js/application.js
@@ -1,45 +1,75 @@
 $(document).ready(function() {
-  dropDown();
+ var events = eventModule()
+ events.dropDown()
+ events.bindFadeCardAnnimation()
 });
 
 // Nav DropDown
+(function() {
 
-
-var dropDown = function() {
-
-"use strict";
-
-  var $menuContainer = $('.dropdown-container');
-  var $eventTrigger = $('#dropdown-listener');
-
-  function displayMenu() {
-    $menuContainer.css('display', 'block');
-  };
-
-  function hideMenu() {
-    $menuContainer.css('display', 'none');
-  }
-
-  function triggerDisplay() {
-    $eventTrigger.mouseenter(displayMenu);
-    $menuContainer.mouseleave(hideMenu);
-  };
-
-
-  triggerDisplay();
-
-};
-
-//  Play Screen Card Annimation On Hover
-
-var bindStartAnnimationEvent = function() {
+  var dropDown = function() {
 
   "use strict";
 
-  var $triggerContainer = $("start-container")
+    var $menuContainer = $('.dropdown-container');
+    var $eventTrigger = $('#dropdown-listener');
 
-}
+    function displayMenu() {
+      $menuContainer.css('display', 'block');
+    };
 
+    function hideMenu() {
+      $menuContainer.css('display', 'none');
+    }
+
+    function triggerDisplay() {
+      $eventTrigger.mouseenter(displayMenu);
+      $menuContainer.mouseleave(hideMenu);
+    };
+
+    triggerDisplay()
+
+  };
+
+  //  Play Screen Card Annimation On Hover
+
+  var bindFadeCardAnnimation = function() {
+
+    "use strict";
+
+    var $triggerContainer = $(".start-container");
+    var $cardContainer = $(".play-card-container")
+
+    function cardsFadeIn() {
+      $cardContainer.fadeIn("slow");
+    };
+
+    function cardsFadeOut() {
+      $cardContainer.fadeOut("slow");
+    }
+
+    function triggerCardFade() {
+      $triggerContainer.mouseenter(cardsFadeIn);
+    };
+
+    function triggerCardFadeOut() {
+      $triggerContainer.mouseleave(cardsFadeOut);
+    }
+
+    $cardContainer.fadeOut("slow")
+    triggerCardFadeOut()
+    triggerCardFade()
+
+  };
+
+  this.eventModule = function() {
+    return {
+      dropDown: dropDown,
+      bindFadeCardAnnimation: bindFadeCardAnnimation
+    }
+  }
+
+})();
 
 // OO
 
